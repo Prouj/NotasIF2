beef7437e40b8bb336cde0cd48a07c98b27f3164
Paulo Rubem <rubem.oliveira27@hotmail.com>
2021-02-08 09:09:30 -0300

    Initial Commit

---
+++ NotaIF2/NotaIF2App.swift
@@ -0,0 +1,20 @@
+//
+//  NotaIF2App.swift
+//  NotaIF2
+//
+//  Created by Paulo Uchôa on 08/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NotaIF2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
